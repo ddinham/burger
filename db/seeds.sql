ea61867ba627392974e272624bf035155b57dc15
Dawn Bondy <dinham.dawn@gmail.com>
2018-07-21 23:51:24 -0400

    new

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('BigMac');
+INSERT INTO burgers (burger_name) VALUES ('BiggieMac');
+INSERT INTO burgers (burger_name, devoured) VALUES ('BaconSupreme', true);
